330365028f0a89a8647fc6e63719c129e58f1244
Ben Sun <sunb26@mcmaster.ca>
2024-12-24 12:45:57 -0500

    add hero component

---
+++ app/components/hero.tsx
@@ -0,0 +1,17 @@
+/**
+ * TODO: Fill in Header text when finalized.
+ * 
+ * Gets hero section of homepage
+ * @returns Hero image component
+ */
+
+export const Hero = () => {
+  return (
+    <section className="relative grid grid-rows-3 grid-cols-3 h-screen bg-cover bg-center bg-hero-pattern">
+      <div className="flex justify-center items-center col-start-1 col-end-2 row-start-2 row-end-3 p-16">
+        <h1 className="text-white text-4xl md:text-6xl font-bold"> 
+        </h1>
+      </div>
+    </section>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/hero.png')",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
