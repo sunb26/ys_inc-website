7edd55137bf9b2d0b3f6138d79e83e8275d7e4c4
Ben Sun <sunb26@mcmaster.ca>
2024-12-25 16:04:04 -0500

    update about section image for mobile

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <section id="about" className="bg-dark-blue h-fit p-4 md:h-[75vh]">
       <div className="flex flex-col md:flex-row w-full h-full pt-8">
-        <Image src="/about.png" alt="About section image" className="container mx-auto md:flex-row md:w-4/12 h-5/6 rounded-xl" width={500} height={500}/>
+        <Image src="/about.png" alt="About section image" className="container mx-auto md:flex-row size-1/2 md:w-4/12 md:h-5/6 rounded-xl" width={500} height={100}/>
         <div className="w-full md:w-1/2 pr-10">
           <h2 className="text-5xl text-white font-bold pl-10 pt-10 underline">About</h2>
           <p className="text-white pl-10 pt-10 text-lg">
