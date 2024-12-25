95bb4bd09e9d0b8f6ab2c3fbd74cebd3caac5c47
Ben Sun <sunb26@mcmaster.ca>
2024-12-25 15:40:10 -0500

    fix showroom for mobile view

--- app/components/showroom.tsx
+++ app/components/showroom.tsx
@@ -10,9 +10,9 @@ export function Showroom() {
 
   return (
     <section id="showroom" className="py-16 bg-slate-100 pt-3">
-      <h2 className="text-5xl font-bold underline text-black text-center pb-8">Showroom</h2>
+      <h2 className="text-5xl font-bold underline text-black text-center p-16">Showroom</h2>
       <div className="text-center bg-slate-100">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-28">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:px-28">
           {models.map((model) => (
             <Link key={model.id} href={`/model/${model.id}`} className="cursor-pointer">
               <div className="bg-white shadow-md rounded-lg p-10 transition-transform transform hover:scale-105">
