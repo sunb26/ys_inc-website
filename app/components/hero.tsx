d197612612a816f7c6ff7af70ec1985ca4fdc474
Ben Sun <sunb26@mcmaster.ca>
2024-12-24 20:37:15 -0500

    update hero image to scale on mobile

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,7 +7,7 @@
 
 export const Hero = () => {
   return (
-    <section id="home" className="relative grid grid-rows-3 grid-cols-3 h-[85vh] w-full bg-cover bg-center bg-hero-pattern rounded-md">
+    <section id="home" className="relative grid grid-rows-3 grid-cols-3 h-[85vh] w-full bg-cover bg-center bg-hero-pattern rounded-md md:bg-cover bg-[length:100%_100%]">
       <div className="flex justify-center items-center col-start-1 col-end-2 row-start-2 row-end-3 pl-16">
         <h1 className="text-white text-4xl md:text-6xl font-bold"> 
         </h1>
