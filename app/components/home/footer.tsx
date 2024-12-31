23215233cedcd1d44ee3c8b9d7b75ce2896081e4
Ben Sun <sunb26@mcmaster.ca>
2024-12-31 12:25:57 -0500

    update footer contact info

--- app/components/home/footer.tsx
+++ app/components/home/footer.tsx
@@ -1,16 +1,16 @@
 export function Footer() {
   return (
-    <footer className="flex flex-wrap gap-x-52 bg-dark-blue text-white p-4">
-      <div className="text-xl font-bold pl-64">LOGO</div>
+    <footer className="flex flex-wrap space-x-44 bg-dark-blue text-white p-4">
+      <div className="text-xl font-bold pl-56">LOGO</div>
       <div className="flex flex-col items-left">
         <h2 className="text-lg font-bold underline">Contact</h2>
-        <p>Email: <a href="mailto:email@domain.com" className="hover:underline">email@domain.com</a></p>
-        <p>Phone: 999-999-9999</p>
+        <p>Email: <a href="mailto:sales@yachtingsolutionsinc.com" className="hover:underline">sales@yachtingsolutionsinc.com</a></p>
+        <p>Phone: 289-241-8164</p>
       </div>
       <div className="flex flex-col items-left">
-              <h2 className="text-lg font-bold underline">Location</h2>
-              <p >St Catharines, Ontario,<br />Canada</p>
-            </div>
+        <h2 className="text-lg font-bold underline">Location</h2>
+        <p >St Catharines, Ontario,<br />Canada</p>
+        </div>
       <div className="flex flex-col items-left">
         <h2 className="text-lg font-bold underline">Socials</h2>
         <p>Facebook Icon</p>
