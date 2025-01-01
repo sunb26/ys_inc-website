9e8e7f1eb96a221284cb52f8b44174df17d338a4
Ben Sun <sunb26@mcmaster.ca>
2025-01-01 00:40:14 -0500

    updated domains [deprecated]

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['drive.google.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      }
+    ]
   },
 };
 
