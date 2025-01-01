c4d8788fd8bfa438b485f30dd96913753631696f
Ben Sun <sunb26@mcmaster.ca>
2025-01-01 14:16:38 -0500

    home about section layout changes

--- app/components/home/about.tsx
+++ app/components/home/about.tsx
@@ -6,8 +6,8 @@ export function About() {
       <div className="flex flex-col md:flex-row w-full h-full pt-8">
         <Image src="/about.avif" alt="About section image" className="container mx-auto md:flex-row size-1/2 md:w-min md:h-5/6 rounded-xl" width={500} height={100}/>
         <div className="w-full md:w-1/2 pr-10">
-          <h2 className="text-5xl text-white font-bold pl-10 pt-10 underline">About</h2>
-          <p className="text-white pl-10 pt-10 text-lg">
+          <h2 className="text-5xl text-white font-bold pl-10 underline">About</h2>
+          <p className="text-white pl-10 pt-6 text-lg">
             Yachting Solutions Inc. primarily specializes in the purchasing and selling of late models Chris Craft sport boats and other high end sport boats in the 20 to 32 foot range.
             Our services include refurbishing of older Chris Craft sport boats, and we offer a wide range of services, including but not limited to fiberglass reparis, mechanical services, bright work,
              electrical troubleshooting, upholstery, canvas and structural repairs.
@@ -18,7 +18,6 @@ export function About() {
             <br />
             <br />
             Owner,
-            <br />
             <br />
             Jan Willem de Jong
           </p>
