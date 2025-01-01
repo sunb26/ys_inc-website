4347bcd0e40fbde1029e3c6a87ae30da9e0d975d
Ben Sun <sunb26@mcmaster.ca>
2025-01-01 18:31:10 -0500

    update contact page with email functionality

--- app/components/home/contact.tsx
+++ app/components/home/contact.tsx
@@ -1,26 +1,75 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
+
 export function Contact() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    fetch(`/api/send`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        sender: email,
+        fullName: name,
+        phone,
+        message,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert('Message sent successfully!');
+          setName('');
+          setEmail('');
+          setPhone('');
+          setMessage('');
+        } else {
+          console.log(res);
+          alert('Something went wrong, please try again later.');
+        }
+      })
+      .catch(() => {
+        alert('Something went wrong, please try again later.');
+      });
+  }
+
   return (
     <section id="contact" className="flex flex-row bg-slate-100 text-white py-12">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row gap-32">
             <div>
-              <form className="space-y-4 w-5/6">
+              <form onSubmit={onSubmit} className="space-y-4 w-5/6">
                 <input
+                  value={name}
+                  onChange={ e => setName(e.target.value) }
                   type="text"
                   placeholder="Name"
                   className="w-full p-3 border border-gray-300 rounded text-black"
                 />
                 <input
+                  value={email}
+                  onChange={ e => setEmail(e.target.value) }
                   type="email"
                   placeholder="Email"
                   className="w-full p-3 border border-gray-300 rounded text-black"
                 />
                 <input
+                  value={phone}
+                  onChange={ e => setPhone(e.target.value) }
                   type="phone"
                   placeholder="Phone Number"
                   className="w-full p-3 border border-gray-300 rounded text-black"
                 />
                 <textarea
+                  value={message}
+                  onChange={ e => setMessage(e.target.value) }
                   placeholder="Message"
                   className="w-full p-3 border border-gray-300 rounded text-black h-32"
                 ></textarea>
