cb61ae1567df8afa20f09d94c58477f9b7b78f31
Ben Sun <sunb26@mcmaster.ca>
2025-01-01 21:24:45 -0500

    disable link href if 'coming soon' + prettier

--- app/api/getModel/[modelId]/route.ts
+++ app/api/getModel/[modelId]/route.ts
@@ -18,7 +18,7 @@ export async function GET(
       return new Response("Invalid model ID", { status: 400 });
     }
 
-    const range = `Sheet1!A${mid + 1}:D${mid + 1}`;
+    const range = `Sheet1!A${mid + 1}:E${mid + 1}`;
 
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SHEET_ID!,
@@ -29,14 +29,12 @@ export async function GET(
       return NextResponse.json({ status: 404, body: "model data not found" });
     }
 
-    const [name, description, highlights, specifications] =
+    const [name, description, highlights, specifications, enabled] =
       response.data.values[0];
-
-    console.log(response.data.values[0]);
     
     return NextResponse.json({
       status: 200,
-      body: { name, description, highlights, specifications },
+      body: { name, description, highlights, specifications, enabled },
     });
   } catch (error) {
     return NextResponse.json({ status: 500, body: error });
--- app/components/home/showroom.tsx
+++ app/components/home/showroom.tsx
@@ -1,20 +1,28 @@
-import Link from 'next/link';
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 export async function Showroom() {
   const ids = [1, 2, 3];
 
   // Fetch all models in parallel
   const modelPromises = ids.map(async (id) => {
-    const infoRes = await fetch(`${process.env.NEXT_DEV_BASE_URL}/api/getModel/${id}`);
+    const infoRes = await fetch(
+      `${process.env.NEXT_DEV_BASE_URL}/api/getModel/${id}`
+    );
     if (!infoRes.ok) {
-      throw new Error(`showroom: error fetching model ${id}: ${infoRes.statusText}`);
+      throw new Error(
+        `showroom: error fetching model ${id}: ${infoRes.statusText}`
+      );
     }
     const data = await infoRes.json();
 
-    const imgRes = await fetch(`${process.env.NEXT_DEV_BASE_URL}/api/listImages/${id}?pageSize=1`);
+    const imgRes = await fetch(
+      `${process.env.NEXT_DEV_BASE_URL}/api/listImages/${id}?pageSize=1`
+    );
     if (!imgRes.ok) {
-      throw new Error(`showroom: error fetching images for model ${id}: ${imgRes.statusText}`);
+      throw new Error(
+        `showroom: error fetching images for model ${id}: ${imgRes.statusText}`
+      );
     }
     const images = await imgRes.json();
     if (!images.body || images.body.length === 0) {
@@ -28,20 +36,39 @@ export async function Showroom() {
 
   return (
     <section id="showroom" className="py-16 bg-slate-100 pt-3">
-      <h2 className="text-5xl font-bold underline text-black text-center p-16">Showroom</h2>
+      <h2 className="text-5xl font-bold underline text-black text-center p-16">
+        Showroom
+      </h2>
       <div className="text-center bg-slate-100">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:px-28">
-          {models.map((model) => (
-            <Link key={model.id} href={`/model/${model.id}`} className="cursor-pointer">
-              <div className="bg-white shadow-md rounded-lg p-10 transition-transform transform hover:scale-105">
-                <Image src={model.coverImgSrc} alt="" className="rounded-lg mb-4 border-2 border-dark-blue max-h-[40vh]" width={500} height={500}/>
-                <h3 className="text-lg font-semibold text-black">{model.data.body.name}</h3>
-                <p className="text-gray-600 text-left">{model.data.body.description}</p>
-              </div>
-            </Link>
-          ))}
+          {models.map((model) => {
+            const isEnabled = model.data.body.enabled === "TRUE";
+            return (
+              <Link
+                key={model.id}
+                href={isEnabled ? `/model/${model.id}` : `#showroom`}
+                className="cursor-pointer"
+              >
+                <div className="bg-white shadow-md rounded-lg p-10 transition-transform transform hover:scale-105">
+                  <Image
+                    src={model.coverImgSrc}
+                    alt="Boat Model Cover Image"
+                    className="rounded-lg mb-4 border-2 border-dark-blue max-h-[40vh]"
+                    width={500}
+                    height={500}
+                  />
+                  <h3 className="text-lg font-semibold text-black">
+                    {model.data.body.name}
+                  </h3>
+                  <p className="text-gray-600 text-left">
+                    {model.data.body.description}
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </section>
   );
-};
+}
