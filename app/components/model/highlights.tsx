7e1bee957d82a4b51deb2dac95a5462564ecbd07
Ben Sun <sunb26@mcmaster.ca>
2025-01-02 19:53:48 -0500

    adjust title placement

--- app/components/model/highlights.tsx
+++ app/components/model/highlights.tsx
@@ -5,7 +5,7 @@ interface HighlightsProps {
 export function Highlights({ highlights = [] } : HighlightsProps) {
   return (
     <section className={`bg-dark-blue`}>
-      <h2 className="text-white p-8 text-left text-5xl font-bold underline">Highlights</h2>
+      <h2 className="text-white p-8 pl-10 md:pl-28 text-left text-5xl font-bold underline">Highlights</h2>
       <ul className="px-32 text-white list-disc text-xl">
         {highlights.map(( highlight, index) => (
           <li key={index} className="py-4">{highlight}</li>
--- app/components/model/specifications.tsx
+++ app/components/model/specifications.tsx
@@ -2,7 +2,7 @@
 export function Specifications({ specs = [] }) {
   return (
     <section className={`bg-slate-100`}>
-      <h2 className="text-dark-blue p-8 text-left text-5xl font-bold underline">Specifications</h2>
+      <h2 className="text-dark-blue p-8 pl-10 md:pl-28 text-left text-5xl font-bold underline">Specifications</h2>
       <p>{specs}</p>
     </section>
   );
