ea29225359f771f62011c48eb2d692c4c96e0c04
Ben Sun <sunb26@mcmaster.ca>
2025-01-02 22:12:53 -0500

    lint fix

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,27 +1,32 @@
-import { Resend } from 'resend'
-import { NextRequest, NextResponse } from 'next/server'
-import { EmailTemplate } from '../../components/email/email-template'
+import { Resend } from "resend";
+import { NextRequest, NextResponse } from "next/server";
+import { EmailTemplate } from "@/components/email/email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req : NextRequest) {
+export async function POST(req: NextRequest) {
   try {
-    const { sender, fullName, phone, message } = await req.json()
+    const { sender, fullName, phone, message } = await req.json();
 
     const { data, error } = await resend.emails.send({
-      from: 'Admin <onboarding@resend.dev>',
-      to: ['sales@yachtingsolutionsinc.com'],
-      subject: 'New Message from Yachting Solutions Inc. Website',
-      react: EmailTemplate({ sender, fullName, phone, message }) as React.ReactElement,
+      from: "Admin <onboarding@resend.dev>",
+      to: ["sales@yachtingsolutionsinc.com"],
+      subject: "New Message from Yachting Solutions Inc. Website",
+      react: EmailTemplate({
+        sender,
+        fullName,
+        phone,
+        message,
+      }) as React.ReactElement,
     });
 
     if (error) {
-      console.log(error)
-      return NextResponse.json( { error }, { status: 500 } );
+      console.log(error);
+      return NextResponse.json({ error }, { status: 500 });
     }
 
-    return NextResponse.json( { data }, { status: 200 } );
+    return NextResponse.json({ data }, { status: 200 });
   } catch (error) {
-    return NextResponse.json( { error }, { status: 500 } );
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
